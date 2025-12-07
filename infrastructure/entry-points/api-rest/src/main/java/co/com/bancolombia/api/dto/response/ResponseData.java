68f85b457ea365a1e899d9d351cbc3e9d46911de
Sebastian Baez <Sebastian.Baez@segurossura.com.mx>
2025-12-07 16:08:34 -0500

    refactor: improve th GlobalExceptionHandler declaring status for each exception type, and declare code,message to default

---
+++ infrastructure/entry-points/api-rest/src/main/java/co/com/bancolombia/api/dto/response/ResponseData.java
@@ -0,0 +1,14 @@
+package co.com.bancolombia.api.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseData<T> {
+    private T data;
+    private int status;
+    private String code;
+}
